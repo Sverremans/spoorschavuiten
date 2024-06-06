ae722a0b7636f7090eb7d2586b8a84f84472241b
Sverremans <Sverre@5kanjers.nl>
2024-06-06 16:45:53 +0200

    Kleine aanpassing

--- classes.py
+++ classes.py
@@ -4,6 +4,10 @@ class Station:
         self._x = x
         self._y = y
 
+    def __repr__(self):
+        return self._name
+    
+
 class Connection:
     def __init__(self, stationA: str, stationB: str, dist: int) -> None:
         self._stationA = stationA
