9ca7a6cbf84495bc5bcebf9955c686fd1143e030
Sverremans <Sverre@5kanjers.nl>
2024-06-07 15:22:30 +0200

    last changes

--- spoorschavuiten/visualization/visualize.py
+++ spoorschavuiten/visualization/visualize.py
@@ -6,20 +6,23 @@ import spoorschavuiten.code.main_script as ms
 plt.style.use('_mpl-gallery')
 
 # # make the data
+sizes = 10 # np.random.uniform(15, 80)
+colors = "black"
 
 for station in ms.holland._stations:
     x = station._x
     y = station._y
+
+    fig, ax = plt.subplots()
+
+    ax.scatter(x, y, s=sizes, c=colors)
 # np.random.seed(3)
 # x = 1
 # y = 4
 # size and color:
-sizes = 10 # np.random.uniform(15, 80)
-colors = "black" # np.random.uniform(15, 80)
+ # np.random.uniform(15, 80)
 # plot
-fig, ax = plt.subplots()
 
-ax.scatter(x, y, s=sizes, c=colors)
 
 ax.set(xlim=(0, 60), xticks=np.arange(0, 60),
        ylim=(0, 60), yticks=np.arange(0, 60))
