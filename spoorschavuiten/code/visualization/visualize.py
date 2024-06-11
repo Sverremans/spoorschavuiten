05d398c64300efa4d371df88675a2c6cf7306504
Sverremans <Sverre@5kanjers.nl>
2024-06-11 16:40:05 +0200

    Nederland en connecties worden in een kaart getekent

--- spoorschavuiten/code/visualization/visualize.py
+++ spoorschavuiten/code/visualization/visualize.py
@@ -1,60 +1,58 @@
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-import numpy as np
 from code.classes.classes import Region
 
-def visualizeMap():
+def makeMap(region: Region) -> None:
     '''
     All stations and connections are imported, stations are made to a dot on the plot.
     Connections are made to be a line between dots on the plot.  
     '''
-    holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
-    netherlands = Region("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv")
 
     plt.style.use('_mpl-gallery')
-    
+    plt.figure(figsize=(6,7.5))
+    xConnection = []
+    yConnection = []
+
 # Coordinates Holland
     x = []
     y = []
+    drawDots(region, x, y)
+    drawLines(region, xConnection, yConnection)
+
 # Coordinates Netherlands
-    xn = []
-    yn = []
-    xConnection = []
-    yConnection = []
-    plt.figure(figsize=(6,7.5))
-
-    drawDots(holland, x, y)
-    drawLines(holland, xConnection, yConnection)
-
-    # Coordinates Netherlands
-    xn = []
-    yn = []
-    drawDots(netherlands, xn, yn)
-    drawLines(netherlands, xConnection, yConnection)
-
-    
-    
-    
-
-    
+    # xn = []
+    # yn = []
+    # drawDots(region, xn, yn)
+    # drawLines(region, xConnection, yConnection)
 
     plt.grid(False)
+
+
+def visualizeMap(region: Region) -> None:
+    '''
+    Draws a map of the train network of the Netherlands.
+    '''
+    # makeMap(region)
+    # drawUsedConnections(region)
     plt.savefig('verbindingenInNederland.png')
     plt.show()
 
 
-
-
-
-def drawDots(region: Region, x: list, y: list):
+def drawDots(region: Region, x: list, y: list) -> None:
+    '''
+    Draw all stations as dots on the map.
+    '''
     for station in region._stations:
         x.append(region._stations[station]._x)
         y.append(region._stations[station]._y)
     plt.scatter(x, y, c= "black", s=30)
 
 
-def drawLines(region: Region, xList: list, yList: list):
+def drawLines(region: Region, xList: list, yList: list) -> None:
+    '''
+    Draw all connections as lines on the map.
+    '''
     for connection in region._connections:
         xA = connection._stationA._x
         yA = connection._stationA._y
@@ -65,7 +63,27 @@ def drawLines(region: Region, xList: list, yList: list):
         yList.append(yA)
         xList.append(xB)
         yList.append(yB)
-        plt.plot(xList, yList, c = "gray")
+        plt.plot(xList, yList, "-", c = "gray")
 
         xList.clear()
         yList.clear()
+
+def drawUsedConnections(region: Region, xList: list, yList: list, color: str) -> None:
+# def drawUsedConnections(region: Region) -> None:
+    xList = []
+    yList = []
+    for route in region._routes:
+        for connection in route._route:
+            xA = connection._stationA._x
+            yA = connection._stationA._y
+            xB = connection._stationB._x
+            yB = connection._stationB._y
+
+            xList.append(xA)
+            yList.append(yA)
+            xList.append(xB)
+            yList.append(yB)
+            plt.plot(xList, yList, c = color)
+            # print(color)
+            xList.clear()
+            yList.clear()
--- spoorschavuiten/main_visualisation.py
+++ spoorschavuiten/main_visualisation.py
@@ -1,3 +1,24 @@
-from code.visualization.visualize import visualizeMap
+from code.visualization.visualize import makeMap, visualizeMap, drawUsedConnections
+from code.classes.classes import Region
+from code.algorithms import randomise
 
-visualizeMap()
+holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
+
+max_trains = 7
+makeMap(holland)
+xConnections = []
+yConnections = []
+colors = ["red", "blue", "pink", "black", "yellow", "hotpink", "orange"]
+
+for i in range(max_trains):
+    randomise.random_route_2(holland)
+    if holland.is_solution():
+        break
+    drawUsedConnections(holland, xConnections, yConnections, colors[i])
+    # print("-----------------------kleurtje-----------------------")
+    # print(colors[i])
+    # print("-----------------------kleurtje-----------------------")
+
+visualizeMap(holland)
+holland.generate_output()
+
