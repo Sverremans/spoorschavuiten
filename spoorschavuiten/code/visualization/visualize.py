46a12dab4eb97c21249b05f23116d86c4e62c3fa
Sverremans <Sverre@5kanjers.nl>
2024-06-11 11:20:01 +0200

    test

--- spoorschavuiten/code/visualization/visualize.py
+++ spoorschavuiten/code/visualization/visualize.py
@@ -5,6 +5,10 @@ import numpy as np
 from code.classes.classes import Region
 
 def visualizeMap():
+    '''
+    All stations and connections are imported, stations are made to a dot on the plot.
+    Connections are made to be a line between dots on the plot.  
+    '''
     holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
     netherlands = Region("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv")
 
@@ -19,6 +23,9 @@ def visualizeMap():
     xn = []
     yn = []
 
+    plt.figure(figsize=(6,7.5))
+
+    # x, y = makeDots(holland, x,  )
     for station in holland._stations:
         x.append(holland._stations[station]._x)
         y.append(holland._stations[station]._y)
@@ -26,6 +33,10 @@ def visualizeMap():
     for station in netherlands._stations:
         xn.append(netherlands._stations[station]._x)
         yn.append(netherlands._stations[station]._y)
+
+
+
+
 
     plt.scatter(xn, yn, c= color2, s= sizes)
     plt.scatter(x, y, c= color1, s=sizes)
@@ -47,8 +58,16 @@ def visualizeMap():
 
         xConnection.clear()
         yConnection.clear()
+    plt.grid(False)
+    plt.savefig('verbindingenInNederland.png')
     plt.show()
 
-    plt.savefig('verbindingenInNederland.png')
+    
     print(len(xConnection))
     print(len(yConnection))
+
+def makeDots(region, x, y):
+    for station in region._stations:
+        x.append(region._stations[station]._x)
+        y.append(region._stations[station]._y)
+    return x, y
