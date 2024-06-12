b5151add4fec3e6eb8350b62833975324aa10799
Sipke de Boer <sipke.d.boer@gmail.com>
2024-06-12 16:04:44 +0200

    up to date

--- spoorschavuiten/main_script.py
+++ spoorschavuiten/main_script.py
@@ -1,10 +1,11 @@
 from code.classes.classes import Region
 from code.algorithms import randomise, randomise2
-from code.visualization.visualize import visualizeMap
+#from code.visualization.visualize import visualizeMap
 
 if __name__ == "__main__":
 
     holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
+    nederland = Region("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv")
 
     max_trains = 7
 
