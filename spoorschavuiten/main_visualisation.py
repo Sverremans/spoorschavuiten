d8b61cf40a1f7f14d0d9bf6ebfdd5e79970eec64
Sverremans <Sverre@5kanjers.nl>
2024-06-14 16:01:02 +0200

    Visualisatie doet het weer

--- spoorschavuiten/main_visualisation.py
+++ spoorschavuiten/main_visualisation.py
@@ -6,32 +6,24 @@ from code.algorithms.randomWithConstraints import Random
 holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
 schedule = Schedule(holland)
 max_trains = 7
-# xConnections = []
-# yConnections = []
 colors = ["red", "blue", "pink", "black", "yellow",
         "hotpink", "orange", "violet", "cyan", "purple",
         "maroon", "indigo", "teal", "magenta", "crimson", "palevioletred", 
         "salmon", "deepskyblue", "deeppink", "darkviolet", "goldenrod"]
 
-# for i in range(max_trains):
-#     randomise2.random_route_2(holland)
-#     if holland.is_solution():
-#         break
-
-# randomWithConstraints.createRandomSolution(holland, 120, 7)
 random = Random(schedule, 120, 7)
 random.run()
 
 schedule.generate_output()
-makeMapWithNames(random)
-drawUsedConnections(random, colors)
-visualizeMap(random, "figures/connectionsWithStationNames.png")
+makeMapWithNames(schedule)
+drawUsedConnections(schedule, colors)
+visualizeMap(schedule, "figures/connectionsWithStationNames.png")
 
-# makeMap(holland)
-# drawUsedConnections(holland, colors)
-# visualizeMap(holland, "figures/connectionsInTheNetherlands.png")
+makeMap(schedule)
+drawUsedConnections(schedule, colors)
+visualizeMap(schedule, "figures/connectionsInTheNetherlands.png")
 
-# onlyLines(holland)
-# drawUsedConnections(holland, colors)
-# visualizeMap(holland, "figures/onlyLines.png")
-# print(len(colors))
+onlyLines(schedule)
+drawUsedConnections(schedule, colors)
+visualizeMap(schedule, "figures/onlyLines.png")
+print(len(colors))
