e6c52996501f99602c6cb9eeff3f45d83600a124
massimocarbone12 <massimocarbone12@hotmail.com>
2024-06-19 16:53:18 +0200

    to save output depthfirst

---
+++ spoorschavuiten/run_depthfirst.py
@@ -0,0 +1,13 @@
+from code.algorithms.depth_first import DepthFirst
+from code.classes.classes import Schedule, Region
+from code.visualization.visualize import *
+
+holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
+new_schedule = Schedule(holland)
+max_trains = 4
+max_time = 120
+
+depth_first = DepthFirst(new_schedule, max_time, max_trains)
+depth_first.run()
+
+outputToFile(depth_first.schedule, "Depht First algorithm output, 19/6/2024", "data/depthfirst_schedule.csv")
