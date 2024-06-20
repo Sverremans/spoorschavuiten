1236635ea4ecac47cc8fe6bb0ed3db86aad4b71c
Sverremans <Sverre@5kanjers.nl>
2024-06-20 13:51:21 +0200

    Testen gedaan om het algoritme lang te laten lopen

--- spoorschavuiten/run_hillclimber.py
+++ spoorschavuiten/run_hillclimber.py
@@ -5,22 +5,23 @@ from code.visualization.visualize import *
 
 
 holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
-schedule = Schedule(holland)
+netherlands = Region("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv")
+schedule = Schedule(netherlands)
 
-random = Random(schedule, 120, 7)
+random = Random(schedule, 180, 20)
 random.run()
 
-iterations = 10
+iterations = 5
 
 for i in range(iterations):
     # hillClimber = HcStopCondition(schedule, 120, 7, 1000000)
     # hillClimber.run(10, 4)
 
-    hillClimber = HillClimber(schedule, 120, 7)
-    hillClimber.run(1000, 4)
+    hillClimber = HillClimber(schedule, 180, 20)
+    hillClimber.run(1000000, 12)
 
-    outputToFile(hillClimber._newSchedule, f"test_{i + 1}", "data/test123.csv")
-    routesToFile(hillClimber._newSchedule, f"test{i + 1}", "data/test456.csv")
-    outputHillClimberGraph(hillClimber.iterations_listPoints, hillClimber.scoresPoints, hillClimber.iterations_list, hillClimber.scores, f"test{i + 1}", "data/test789.csv")
+    outputToFile(hillClimber._newSchedule, f"Nederland test_{i + 1}", "data/test123.csv")
+    routesToFile(hillClimber._newSchedule, f"Nederland test_{i + 1}", "data/test456.csv")
+    outputHillClimberGraph(hillClimber.iterations_listPoints, hillClimber.scoresPoints, hillClimber.iterations_list, hillClimber.scores, f"Nederland test_{i + 1}", "data/test789.csv")
 
     # raise NotImplementedError
