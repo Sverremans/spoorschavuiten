0735756363b6afc94964c701658d61960c181a6c
Sverre <Sverre@5kanjers.nl>
2024-06-23 18:28:18 +0200

    Hele hoop data verkregen

--- spoorschavuiten/distribution.py
+++ spoorschavuiten/distribution.py
@@ -1,41 +1,76 @@
 from code.classes.classes import Region, Schedule
 # from code.algorithms import randomise, randomise2, randomWithConstraints
 from code.algorithms import random as rd
+from code.algorithms import greedy as gd
 from code.visualization.visualize import *
 import time
 
+colors = ["red", "blue", "pink", "black", "yellow",
+        "hotpink", "orange", "violet", "cyan", "purple",
+        "maroon", "indigo", "teal", "magenta", "crimson", "palevioletred", 
+        "salmon", "deepskyblue", "deeppink", "darkviolet", "goldenrod"]
 
 if __name__ == "__main__":
 
     holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
+    netherlands = Region("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv") 
 
     start = time.time()
     all_outputs = []
     times = 1000000
     timeList = []
-    colors = ["red", "blue", "pink", "black", "yellow",
-        "hotpink", "orange", "violet", "cyan", "purple",
-        "maroon", "indigo", "teal", "magenta", "crimson", "palevioletred", 
-        "salmon", "deepskyblue", "deeppink", "darkviolet", "goldenrod"]
 
     for i in range(times):
         timeList.append(i + 1)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # # Random Holland
+    # for i in range(times):
+    #     schedule = Schedule(holland)
+    #     randomSolution = rd.Random(schedule, 120, 7)
+    #     randomSolution.run()
 
+    #     output = randomSolution.schedule.calculate_value()
+    #     scoresToFile(schedule, i, "data/1000000RandomScores.csv")
+    #     # print(schedule.calculate_value())
+    #     all_outputs.append(output)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # # Random Nederland
+    # for i in range(times):
+    #     schedule = Schedule(netherlands)
+    #     randomSolution = rd.Random(schedule, 180, 20)
+    #     randomSolution.run()
+
+    #     output = randomSolution.schedule.calculate_value()
+    #     scoresToFile(schedule, i, "data/1000000RandomScoresNederland.csv")
+    #     # print(schedule.calculate_value())
+    #     all_outputs.append(output)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Greedy Holland
     for i in range(times):
         schedule = Schedule(holland)
-        randomSolution = rd.Random(schedule, 120, 7)
-        randomSolution.run()
+        greedySolutionHol = gd.Greedy(schedule, 120, 7)
+        greedySolutionHol.run()
 
-        output = randomSolution.schedule.calculate_value()
-        scoresToFile(schedule, i, "data/testen.csv")
+        output = greedySolutionHol.schedule.calculate_value()
+        scoresToFile(schedule, i, "data/1000000GreedyScoresHolland.csv")
         # print(schedule.calculate_value())
         all_outputs.append(output)
 
-    end = time.time()
-    # outputGraph(all_outputs, timeList)
-    # print(timeList)
-    # print(all_outputs)
-    outputGraphHist(all_outputs, timeList)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Greedy Nederland
+    for i in range(times):
+        schedule = Schedule(netherlands)
+        greedySolutionNed = gd.Greedy(schedule, 180, 20)
+        greedySolutionNed.run()
 
-    
+        output2 = greedySolutionNed.schedule.calculate_value()
+        scoresToFile(schedule, i, "data/1000000GreedyScoresNederland.csv")
+        # print(schedule.calculate_value())
+        all_outputs.append(output2)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    end = time.time()
     print(end - start)
--- spoorschavuiten/run_depthfirst.py
+++ spoorschavuiten/run_depthfirst.py
@@ -3,16 +3,18 @@ from code.algorithms.greedy_lookahead import GreedyLookahead
 from code.classes.classes import Schedule, Region
 from code.visualization.visualize import *
 
-holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
-new_schedule = Schedule(holland)
-max_trains = 7
-max_time = 120
+# holland = Region("data/StationsHolland.csv", "data/ConnectiesHolland.csv")
+netherlands = Region("data/StationsNationaal.csv", "data/ConnectiesNationaal.csv") 
+# new_schedule = Schedule(holland)
+new_schedule = Schedule(netherlands)
+max_trains = 20
+max_time = 180
 
 greedy_lookahead = GreedyLookahead(new_schedule, max_time, max_trains, 3)
 greedy_lookahead.run()
 
-outputToFile(greedy_lookahead.schedule, "Greedy Lookahead algorithm output, 21/6/2024", "data/greedylookahead_ouput.csv")
-routesToFile(greedy_lookahead.schedule, "Greedy Lookahead algorithm output, 21/6/2024", "data/greedylookahead_route.csv")
+outputToFile(greedy_lookahead.schedule, "Greedy Lookahead algorithm output, 23/6/2024", "data/greedylookahead_Nederland_ouput.csv")
+routesToFile(greedy_lookahead.schedule, "Greedy Lookahead algorithm output, 23/6/2024", "data/greedylookahead_Nederland_route.csv")
 
 
 # depth_first = DepthFirst(new_schedule, max_time, max_trains)
